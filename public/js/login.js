22e60013e66d13395229d0130ff2a186197b83e0
Gabriel Augusto <gabrielaugustoassisnascimento@gmail.com>
2025-06-15 23:03:35 -0300

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ button.addEventListener('click', async (event) => { // Use async event handler
     }
 
     try {
-        const response = await fetch('http://localhost:3000/auth/login', {
+        const response = await fetch('https://backendtcc.vercel.app/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
