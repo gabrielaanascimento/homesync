df279e228ab9adb96e25d80bd3f2ad989bfd50b1
Gabriel Augusto <gabrielaugustoassisnascimento@gmail.com>
2025-11-16 02:05:23 -0300

    Correção

--- src/components/profile/Products/index.tsx
+++ src/components/profile/Products/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import { ItemProducts } from "../ItemProduct";
+import { ItemProducts } from "../ItemProducts";
 
 interface Property {
   id: number; // 1. ADICIONAR ID AQUI
